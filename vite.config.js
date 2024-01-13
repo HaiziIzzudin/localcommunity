86fc96d308c8407916dff9f39521349405fff6e4
Haizi <thehub@iziizz.slmail.me>
2024-01-13 12:25:06 +0800

    adjustments

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,40 @@
 // vite.config.js
-import { resolve } from 'path'
+// import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-    base: '/localcommunity/',
+// export default defineConfig([
+//     {
+//         input: 'index.html',
+//         output: { dir: './' },
+//         plugins: [html()],
+//     },
+//     {
+//         input: 'about.html',
+//         output: { dir: './' },
+//         plugins: [html()],
+//     },
+// ], {
+//     base: '/localcommunity/',
+// });
 
+export default defineConfig({
     build: {
-        rollupOptions: {
-            input: 'index.html',
-            input: 'about.html',
-            input: 'amenities.html',
-            input: 'attractions.html',
-            input: 'contacts.html',
-            input: 'newsroom.html',
-            input: 'staff.html',
+    // outDir: 'dist',
+
+    rollupOptions: {
+        input: {
+            index: fileURLToPath(new URL('index.html', import.meta.url)),
+            about: fileURLToPath(new URL('about.html', import.meta.url)),
+            amenities: fileURLToPath(new URL('amenities.html', import.meta.url)),
+            attractions: fileURLToPath(new URL('attractions.html', import.meta.url)),
+            contacts: fileURLToPath(new URL('contacts.html', import.meta.url)),
+            newsroom: fileURLToPath(new URL('newsroom.html', import.meta.url)),
+            staff: fileURLToPath(new URL('staff.html', import.meta.url)),
         },
     },
-})
+    },
+},
+{
+    base: '/localcommunity/',
+});
