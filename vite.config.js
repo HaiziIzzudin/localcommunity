93e7378489c5040ab9a4d8fd9c71a5114b04ce26
Haizi <thehub@iziizz.slmail.me>
2024-01-13 11:59:52 +0800

    ddd

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,21 @@
+// vite.config.js
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
     base: '/localcommunity/',
+
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                main: resolve(__dirname, 'about.html'),
+                main: resolve(__dirname, 'amenities.html'),
+                main: resolve(__dirname, 'attractions.html'),
+                main: resolve(__dirname, 'contacts.html'),
+                main: resolve(__dirname, 'newsroom.html'),
+                main: resolve(__dirname, 'staff.html'),
+            },
+        },
+    },
 })
