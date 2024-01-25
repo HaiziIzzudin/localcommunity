b58b644e22d1e34d2c856dc0f0912b6f0fc9903e
Haizi <thehub@iziizz.slmail.me>
2024-01-25 18:12:45 +0800

    changes to vite build

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,11 @@ export default defineConfig({
             input: {
                 index: fileURLToPath(new URL('index.html', import.meta.url)),
                 about: fileURLToPath(new URL('about.html', import.meta.url)),
-                amenities: fileURLToPath(new URL('amenities.html', import.meta.url)),
                 attractions: fileURLToPath(new URL('attractions.html', import.meta.url)),
+                amenities: fileURLToPath(new URL('departments.html', import.meta.url)),
+                staff: fileURLToPath(new URL('ahli-majlis.html', import.meta.url)),
                 contacts: fileURLToPath(new URL('contacts.html', import.meta.url)),
                 newsroom: fileURLToPath(new URL('newsroom.html', import.meta.url)),
-                staff: fileURLToPath(new URL('staff.html', import.meta.url)),
             },
         },
     },
